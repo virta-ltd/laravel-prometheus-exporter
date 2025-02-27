491d9bb32df5a8a11291a4f1c9ca1458cc96bf72
Michael Wikberg <michael.wikberg@virta.global>
2025-02-27 14:56:19 +0200

    chore: Run with multiple PHP versions, use cache, record coverage

---
+++ rector.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use RectorLaravel\Set\LaravelSetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/config',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+    ->withTypeCoverageLevel(0)
+    ->withDeadCodeLevel(0)
+    ->withCodeQualityLevel(0)
+    ->withPhpSets()
+    ->withSets([
+        LaravelSetList::LARAVEL_110,
+        LaravelSetList::LARAVEL_CODE_QUALITY,
+    ])
+    ;
